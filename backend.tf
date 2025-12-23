c382fc0acac0577f0ad043be40e043b8788f0d3f
charles <charlez20201010@gmail.com>
2025-12-23 18:10:12 +0000

    initail commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "maryann-terraform-state"
-    key    = "maryann-terraform-state/prodution/terraform.tfstate"
+    bucket = "charles1-bucket"
+    key    = "charles1-bucket/prodution/terraform.tfstate"
     region = "us-east-1"
   }
 }
